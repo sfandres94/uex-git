02f4ac4bf86004f71273e7deb1195c38dff9ae36
adrian <agonzaleog@alumnos.unex.es>
2023-02-23 21:47:20 +0100

    Traduccion de mensajes

--- calculator.py
+++ calculator.py
@@ -1,48 +1,48 @@
 # Custom function to read the two numbers.
 def read_numbers():
-    num1 = float(input('Enter first number: '))
-    num2 = float(input('Enter second number: '))
+    num1 = float(input('Introduce el primer numero: '))
+    num2 = float(input('Introduce el segundo numero: '))
     return num1, num2
 
 # Simple menu.
-print('Welcome to the calculator!')
-print('Please choose an operation:')
-print('    1. Addition')
-print('    2. Subtraction')
-print('    3. Multiplication')
+print('Bienbenido a la calculadora!')
+print('Seleccione operacion:')
+print('    1. Suma')
+print('    2. Resta')
+print('    3. Multiplicacion')
 print('    4. Division')
-print('    5. Exit')
+print('    5. Salir')
 
 # Main loop.
 while True:
 
-    choice = input('\nEnter your choice (1-5): ')
+    choice = input('\nIntroduce tu eleccion (1-5): ')
 
     if choice in ('1', '2', '3', '4', '5'):
         if choice == '1':
             num1, num2 = read_numbers()
             result = num1 + num2
-            print(f'Result: {result}')
+            print(f'Resultado: {result}')
         
         elif choice == '2':
             num1, num2 = read_numbers()
             result = num1 - num2
-            print(f'Result: {result}')
+            print(f'Resultado: {result}')
         
         elif choice == '3':
             num1, num2 = read_numbers()
             result = num1 * num2
-            print(f'Result: {result}')
+            print(f'Resultado: {result}')
         
         elif choice == '4':
             try:
                 num1, num2 = read_numbers()
                 result = num1 / num2
-                print(f'Result: {result}')
+                print(f'Resultado: {result}')
             except ZeroDivisionError:
-                print("Error: division by zero")
+                print("Error: division entre 0")
         
         elif choice == '5':
             break
     else:
-        print('Invalid Input')
+        print('Entrada invalida')
